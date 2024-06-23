fd25a2262914dc0fb6d436867868d5b6fbee3f2e
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-23 12:39:43 +0100

    C pushing out

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
- * followed by a new line, to standard error.
+ * main - Entry point
  *
  * Return: Always 1 (Error)
  */
 int main(void)
 {
-	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
