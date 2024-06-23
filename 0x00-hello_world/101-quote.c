b9535ba8c1f07912e3819a78af7845b68504b39b
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-23 12:32:18 +0100

    C pushing out

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDOUT_FILENO, message, sizeof(message) -1);
 	return (1);
 }
