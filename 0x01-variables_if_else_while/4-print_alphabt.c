57fb53d5c76ad6f2dc90cedbcdc27c281ce94517
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-24 09:58:01 +0100

    C pushing out

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-        char c;
+	char c;
 
-        for (c = 'a'; c <= 'z'; ++c)
+	for (c = 'a'; c <= 'z'; ++c)
 		if (c != 'q' && c != 'e')
-                putchar(c);
-        putchar('\n');
-        return (0);
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
