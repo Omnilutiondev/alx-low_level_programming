9bf495f28fc5d30543a44dfe521e41b5231fc3b5
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-24 15:51:00 +0100

    C pushing out

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int digit = 0; digit < 10; ++digit)
+		putchar('0' + digit);
+	putchar('\n');
+	return (0);
+}
