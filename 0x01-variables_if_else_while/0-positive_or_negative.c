ca25782847fb129305886412c3310e0cac523a59
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-24 05:37:38 +0100

    C pushing out

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,17 +13,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	printf("%d", n);
-
 	if (n > 0)
 	printf(" is positive\n");
-	
 	else if (n == 0)
 	printf(" is zero\n");
-	
 	else
 	printf(" is negative\n");
-	/* your code goes there */
 	return (0);
 }
