63624b06e39e515145144dcdbc01dc1297d2d707
omnilutiondev <omnilutiondev@ULUNMA>
2024-06-25 15:48:48 +0100

    Nested Loops n Push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+
+{
+
+    char start = 'a';
+    char end = 'z';
+
+    while (start <= end)
+    {
+        _putchar(start);
+        start++;
+    }
+
+    _putchar('\n');
+
+}
+
