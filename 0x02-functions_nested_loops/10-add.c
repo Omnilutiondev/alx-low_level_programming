270b81941f18ab357c5a87b7c05697786b514e65
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-27 00:34:01 +0100

    Nested Loops n Push

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Adds two integers
+ * @n1: The first number
+ * @n2: The second number
+ * Return: Integer
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
