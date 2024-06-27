b90790a46a3d789c44b6f99590691f477ad8448d
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-27 23:30:49 +0100

    Nested Loops n Push

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers since 0 up to 9
- * 
- * Return: void
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Return: The numbers from 0 to 9
  */
 
 void print_numbers(void)
+
 {
-	int x; 
+	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
