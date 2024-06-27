0583177e3ba825b3e143dd3e08621613562bfd44
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-27 02:50:00 +0100

    Nested Loops n Push

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of all multiples of 3 or 5 and stops at 1024
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int i, z = 0;
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+		z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
