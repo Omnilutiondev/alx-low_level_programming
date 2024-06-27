7e61fee384ccde49b2c1d6262a59359aceebd1b7
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-28 00:39:19 +0100

    Nested Loops n Push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,15 +4,11 @@
  * more_numbers - Prints 10 times the numbers from 0 to 14 on a new line
  * Return: 10 times of the numbers from 0 through to 14
  */
-
 void more_numbers(void)
-
 {
-	int x; y;
-
-	for (x = 0; x < 10; x++)
+	for (int x = 0; x < 10; x++)
 	{
-	for (y = 0; y <= 14; y++)
+	for (int y = 0; y <= 14; y++)
 	{
 	if (y > 9)
 	{
