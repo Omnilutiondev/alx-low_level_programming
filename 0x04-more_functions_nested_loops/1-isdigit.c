daee860862951331179b2746c903f5054cf7cd10
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-27 22:45:35 +0100

    Nested Loops n Push

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * 1-isdigit - Checks for digits from 0 through to 9
  * @c: The digit is checked
- * Return: 1 if integers are less than 9 and 0 for anything else
+ * Return: 1 if the character is a digit or 0 for anything else
  */
 
 int _isdigit(int c)
