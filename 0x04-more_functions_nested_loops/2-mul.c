49c9e40eaead48b581e7a4fe66fcf00f2b1100fd
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-27 22:58:41 +0100

    Nested Loops n Push

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies the two integers
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+
+{
+	if (a >= 0 && b >= 0)
+	{
+	(a * b >= 0);
+	}
+	return (0);
+}
