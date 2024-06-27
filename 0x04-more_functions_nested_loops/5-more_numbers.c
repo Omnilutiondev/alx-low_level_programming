d9bea242336109f2a9ec801c32c50d1c8bfa98a3
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-28 00:47:36 +0100

    Nested Loops n Push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,9 +5,11 @@
  */
 void more_numbers(void)
 {
-	for (int x = 0; x < 10; x++)
+	int x, x;
+
+	for (x = 0; x < 10; x++)
 	{
-	for (int y = 0; y <= 14; y++)
+	for (y = 0; y <= 14; y++)
 	{
 	if (y > 9)
 	{
