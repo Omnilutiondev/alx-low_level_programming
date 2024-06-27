0cf0da193014b07e8784127a1463536745ae900a
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-27 01:54:06 +0100

    Nested Loops n Push

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n<= 98; n++)
+		for (; n <= 98; n++)
 		{
 			if (n == 98)
 			{
@@ -30,13 +30,16 @@ void print_to_98(int n)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d", n);
-			printf("\n");
-			break;
-		}
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
 		else
 		{
-			printf("%d, " n);
+			printf("%d, ", n);
+		}
 		}
 	}
 }
