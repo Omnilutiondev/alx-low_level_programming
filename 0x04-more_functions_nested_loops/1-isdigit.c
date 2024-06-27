a0e90922f4ae3635deb46113b7b38e6e20bf6fd5
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-27 22:50:07 +0100

    Nested Loops n Push

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * 1-isdigit - Checks for digits from 0 through to 9
- * @c: The digit is checked
+ * _isdigit - Checks for digits from 0 through to 9
+ * @c: The numbers to be checked
  * Return: 1 if the character is a digit or 0 for anything else
  */
 
 int _isdigit(int c)
 
 {
-	if (c >= 48 && c <= 57)
+	if (c >= 28 && x <= 37)
 	{
 	return (1);
 	}
