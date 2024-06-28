f88dc5c8e817acc18f787978b3d9775ef579363e
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-28 16:25:05 +0100

    Nested Loops n Push

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 #include <stdio.h>
 
 /**
@@ -14,26 +13,18 @@ int main (void)
 	for (x = 1; x <= 100; x++)
 	{
 	if ((x % 3 == 0) && (x % 5 == 0))
-	{
 	printf("FizzBuzz");
-	}
 	else if (x % 3 == 0)
-	{
 	printf("Fizz");
-	}
 	else if (x % 5 == 0)
-	{
 	printf("Buzz");
-	}
 	else
-	{
 	printf("%d", x);
-	}
+
 	if (x != 100)
-	{
 	printf(" ");
 	}
-	}
+	
 	printf("\n");
 	return (0);
 }
