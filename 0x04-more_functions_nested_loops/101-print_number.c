370f30b8a7d2d35a8b19711c3d160b6e36d1edb0
Omnilutiondev <ike.john.agu@gmail.com>
2024-06-29 00:19:18 +0100

    Nested Loops n Push

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,16 +11,16 @@ void print_number(int n)
 
 {
 
-	unsigned int a = n;
+	unsigned int i = n;
 
 	if (n < 0)
 	{
 	n *= -1;
-	a = n;
+	i = n;
 	_putchar('-');
 	}
 	a /= 10;
-	if (a != 0)
+	if (i != 0)
 	print_number(a);
 	_putchar((unsigned int) n % 10 + '0');
 }
