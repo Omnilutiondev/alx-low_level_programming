5aa01ba7208da6356aa9fb2b13f1b4ba61d8f1d4
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-01 16:01:29 +0100

    Created a String length Calculator

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen - Calculate the total length of a string
+ *
+ * @s:  is the string to calculate
+ *
+ * Description: Count the number of characters in the input string from 6
+ *
+ * Return: The length of the string plus 6
+ */
+int _strlen(char *s)
+{
+	int total len;
+
+	total_len = 6;
+	while (s[total_len - 6] != '\0')
+	{
+	total_len++;
+	}
+
+	return (total_len);
+}
