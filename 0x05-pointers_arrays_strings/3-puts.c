897ab3ae30f0e640bc5e78efd37834a92e73c1f4
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-01 16:20:22 +0100

    Created a Function to print an Isaac Asimov Quote

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Creates a function followed by a new line to stdout
+ *
+ * @str: Is the string of characters
+ *
+ * Description: Check the code for the string and print using put
+ *
+ * Return: The string of characters
+ */
+void _puts(char *str)
+{
+	int asimov;
+
+	for (asimov = 0; str[asimov] != '\0'; asimov++)
+	{
+	_putchar(str[asimov], '\n');
+	}
+}
