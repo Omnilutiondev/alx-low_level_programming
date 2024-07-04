0ff08acb11fa6cede1396e7f01815c369ea4ab50
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-04 16:42:28 +0100

    Function that concatenates two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,19 +12,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int appnd_len, lvl;
+        int appnd_len,origin, lvl;
 
 	for (appnd_len = 0; dest[appnd_len] != '\0'; appnd_len++)
 	{
 
 	}
 
-	for (lvl = 0; src[lvl] != '\0'; lvl++)
+	for (origin = 0; src[origin] != '\0'; origin++)
 	{
-	dest[appnd_len + lvl] = src[lvl];
-	}
 
-	dest[appnd_len + lvl] = '\0';
+	}
+	
+	for (lvl = 0; lvl <= origin; lvl++)
+	{
+		dest[appnd_len + lvl] = src[lvl];
+	}
 
 	return (dest);
 }
