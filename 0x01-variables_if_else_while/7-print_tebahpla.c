c881d9b9556f5ae5cf483a544bbf1d767053c6ee
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-09 03:13:13 +0100

    This is a function that prints the lowercase alphabet. Technical Backlog Cleanup

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - This program prints the lowercase alphabet in reverse
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char start = 'z';
-	do
-	{
-		putchar(start);
-		start--;
-	}
-	while (start >= 'a');
+	char ltr;
+
+	for (ltr = 'z'; ltr >= 'a'; ltr--)
+		putchar(ltr);
+
 	putchar('\n');
+
 	return (0);
 }
