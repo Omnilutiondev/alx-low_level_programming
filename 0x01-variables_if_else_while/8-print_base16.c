5b8d07bd0e367403a441ed397da62bc44427cf87
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-09 03:18:21 +0100

    This is a function that prints the numbers of base 16 in lowercase. Technical Backlog Cleanup

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char dgt;
+
+	for (dgt = '0'; dgt <= 'j'; dgt++)
+	{
+		if ((dgt <= '9') || (dgt >= 'a'))
+			putchar(dgt);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
