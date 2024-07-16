a94308a2926856d28bbfc0594e31d99790c8dd04
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-16 04:07:36 +0100

    Created the static library found the misisng entry

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for digits from 0 through to 9
+ * @c: The numbers to be checked
+ * Return: 1 if the character is a digit or 0 for anything else
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
