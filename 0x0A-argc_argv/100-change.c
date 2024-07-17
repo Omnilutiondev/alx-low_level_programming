d03adcfefb9258774e9e7aa46ac30076bec0a1a5
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-17 23:20:55 +0100

    This is a program prints the minimum number of coins to make change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,35 +7,41 @@
  * @argc: The number of command lines
  * @argv: The array that holds the command lines
  *
- * Return: 0 On Success
+ * Return: 0 On Success, 1 on error
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int cents, min_coin = 0;
+	int cents, min_coins = 0;
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
-	printf("Error\n");
-	return (1);
+		printf("Error\n");
+		return (1);
 	}
 
-	cents = atoi(agrv[1]);
+	cents = atoi(argv[1]);
 
-	while (cents > 0)
+	if (cents < 0)
 	{
-		if (cents >= 25)
-		cents -= 25;
-		if (cents >= 10)
-		cents -= 10;
-		if (cents >= 5)
-		cents -= 5;
-		if (cents >= 2)
-		cents -= 2;
-		if (cents >= 1)
-		cents -= 1;
-		min_coin += 1;
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", min_coin);
+
+	min_coins += cents / 25;
+	cents %= 25;
+
+	min_coins += cents / 10;
+	cents %= 10;
+
+	min_coins += cents / 5;
+	cents %= 5;
+
+	min_coins += cents / 2;
+	cents %= 2;
+
+	min_coins += cents;
+
+	printf("%d\n", min_coins);
 	return (0);
 }
