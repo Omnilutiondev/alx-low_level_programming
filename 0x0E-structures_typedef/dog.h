80f754295015ce81e8d16ee8cc1d365361b21e93
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-23 05:12:47 +0100

    This is task is to define a new type of dog based on the name age amd owners name

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - This represents a dog with its basic information
+ *
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the dog's owner
+ *
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
