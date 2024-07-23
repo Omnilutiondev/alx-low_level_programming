d0a5710d53d8c6dd4420b5d36555025435513663
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-23 19:15:53 +0100

    This function that prints struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,16 +7,22 @@
  * @d: The pointer to struct dog to print
  *
  * Description: This is the function that prints the members of a struct dog.
- * If an element is NULL, it prints (nil) instead.
  * If d is NULL, it prints nothing.
  */
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	return;
-
-	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	if (d)
+	{
+	if (!((*d).name))
+	printf("Name: (nil)\n");
+	else
+	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	if (!(d->owner))
+	printf("Owner: (nil)\n");
+	else
+	printf("Owneer: %s\n", (*d).owner);
+
+	}
 }
