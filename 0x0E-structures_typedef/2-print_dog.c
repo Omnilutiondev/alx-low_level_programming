75b91229dd1dc71e4b6e0efe8d782e5df1df4826
Omnilutiondev <ike.john.agu@gmail.com>
2024-07-24 05:06:29 +0100

    This function that prints struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,6 +5,10 @@
  * print_dog - This function prints a struct dog
  *
  * @d: The pointer to struct dog to print
+ *
+ * Description: This function prints the members of a struct dog
+ * If an element is NULL, it will print (nil) instead
+ * if d is NULL, it prints nothing.
  */
 
 void print_dog(struct dog *d)
