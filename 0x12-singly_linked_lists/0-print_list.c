43edcda2f31e070eb886e2bc53753f8bfe886002
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-05 00:56:10 +0100

    This function prints all nodes in a single list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+
+/**
+ * print_list - This function prints all elements in a singly linked list
+ * called list_t
+ *
+ * @h: The pointer to the head of the list
+ *
+ * Return: The total number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
