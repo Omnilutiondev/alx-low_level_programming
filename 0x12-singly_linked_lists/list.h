3cdce36dc536278e4a01db7ffb5c47217ee65696
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-05 01:27:33 +0100

    This function prints all nodes in a single list

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t print_list(const list_t *h);
 /**
  * struct list_s - these are singly linked lists
  *
@@ -20,6 +19,9 @@ typedef struct list_s
 	size_t len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+
 size_t list_len(const list_t *h);
 
 list_t *add_node(list_t **head, const char *str);
