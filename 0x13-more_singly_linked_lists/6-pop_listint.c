83b5bf67646975395e01f4b7d9989b2d58bfd34a
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-07 03:58:51 +0100

    Singly linked -  function deketes the head node in a linked list and returns its data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - This function deletes the first nod of a list,
+ * and returns the head node data
+ *
+ * @head: This is the pointer to the list
+ *
+ * Return: The head node data
+ */
+
+int pop_listint(listint_t **head)
+{
+	int idx;
+	listint_t *temp_val;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	idx = *head->idx;
+	temp_val = *head;
+	*head = *head->next;
+	free(temp_val);
+	return (idx);
+}
