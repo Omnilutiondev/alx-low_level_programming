f6fa0bf074d54c9e5efbb9243d932f70caffe84e
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-07 04:12:59 +0100

    Singly linked -  function deketes the head node in a linked list and returns its data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,14 +11,16 @@
 
 int pop_listint(listint_t **head)
 {
-	int i;
 	listint_t *temp_val;
+	int i;
 
 	if (head == NULL || *head == NULL)
 		return (0);
-	i = (*head)->i;
+	
 	temp_val = *head;
+	i = temp_val->i;
 	*head = (*head)->next;
 	free(temp_val);
+
 	return (i);
 }
