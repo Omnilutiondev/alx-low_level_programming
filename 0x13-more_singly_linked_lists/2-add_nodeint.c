4413ad94dfd29992858680093e906e0ba154ed2c
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-07 03:14:49 +0100

    Singly linked -  fixed issue with lists filename affecting task three

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,20 +13,20 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *bnew_node;
 
 	if (head == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-			if (new_node == NULL)
+	bnew_node = malloc(sizeof(listint_t));
+			if (bnew_node == NULL)
 				return (NULL);
 			if (*head == NULL)
-				new_node->next = NULL;
+				bnew_node->next = NULL;
 			else
-				new_node->next = *head;
-			new_node->n = n;
-			*head = new_node;
+				bnew_node->next = *head;
+			bnew_node->n = n;
+			*head = bnew_node;
 
 			return (*head);
 }
