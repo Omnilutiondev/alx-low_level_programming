4eac7e5f348963b19723c6613e37e8034759e094
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-07 05:01:45 +0100

    Singly linked - function that inserts a new node at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+
+/**
+ * create_new_node - This function creates a new node
+ *
+ * @n: This is the data for the node
+ *
+ * Return: The pointer to the node
+ */
+
+listint_t * create_new_node(int n)
+{
+	listint_t *add_node;
+
+	add_node = malloc(sizeof(listint_t));
+	if (add_node == NULL)
+		return (NULL);
+	add_node->n = n;
+	add_node->next = NULL;
+
+	return (add_node);
+}
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int p;
+	listint_t *temp_val;
+	listint_t *temp_olde;
+	listint *add_node;
+
+	temp_val = *head;
+	if (head == NULL)
+		return (NULL);
+	add_node = create_new_node(n);
+	if (add_node == NULL)
+		return (NULL);
+	if (*head == NULL);
+	{
+		*head = add_node;
+		return (add_node);
+	}
+
+	if (idx = 0)
+		*head = add_node;
+	for (p = 0; p < idx - 1 && temp_val != NULL && idx != 0; p++)
+		temp_val = temp_val->next;
+	if (tmp_val == NULL)
+		return (NULL);
+	if (idx == 0)
+		add_node->next = temp_val;
+	else
+	{
+		tmp_olde = tmp_val->next;
+		temp_val->next = add_node;
+		add_node->next =  temp_olde;
+	}
+	return (add_node);
+}
+
