94fecdba2d5f3960dc77947534f10c541d084346
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-07 02:27:35 +0100

    Singly linked core files and task 0 complete

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+/**
+ * print_listint - This function prints all the elements in listint_t
+ *
+ * @h: The head pointer for the list
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
