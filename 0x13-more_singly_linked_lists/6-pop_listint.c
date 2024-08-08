0393268d1143d8ccdc8b500b98965355e430e7ff
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-08 16:12:58 +0100

    Singly linked Pop task

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,26 +1,24 @@
 #include "lists.h"
 
 /**
- * pop_listint - This function deletes the first nod of a list,
- * and returns the head node data
+ * pop_listint - This function deletes the first node of a list
+ * and returns its data
  *
  * @head: This is the pointer to the list
  *
- * Return: int
+ * Return: The int
  */
 
 int pop_listint(listint_t **head)
 {
-	int i;
-	listint_t *temp_val;
+        int idx;
+        listint_t *temp_val;
 
-	if (head == NULL || *head == NULL)
-		return (0);
-
-	i = (*head)->i;
-	tmp_val = *head;
-	*head = (*head)->next;
-	free(temp_val);
-
-	return (i);
+        if (head == NULL || *head == NULL)
+                return (0);
+        idx = *head->idx;
+        temp_val = *head;
+        *head = *head->next;
+        free(temp);
+        return (idx);
 }
