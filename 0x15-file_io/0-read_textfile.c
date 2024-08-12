b1070303f0b47f899109ad1b572b8b839667bf31
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-12 22:35:08 +0100

    C-file i/o files and task zero - Tread lightly

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - This function reads a text file and prints
+ * it to the POSIX std output
+ *
+ * @filename: The filename (pointer string)
+ * @letters: The total number of letters printed
+ *
+ * Return: The number of letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int trd;
+	ssize_t ner, she;
+	char *buff;
+
+	if (!filename)
+		return (0);
+
+	trd = p[em(filename, O_RDONLY);
+
+	if (trd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	ner =  read(trd, buff, letters);
+	she = write(STDOUT_FILENO, buff, trd);
+
+	close(trd);
+	free(buff);
+
+	return (she);
+}
