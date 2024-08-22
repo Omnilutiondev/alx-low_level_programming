d1de2dc80a7b6fba38763f6c10cc1345dbeef0bc
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-23 00:33:32 +0100

    write a program that prints all possible combinations of two two digit numbers - task fixed

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - This is the entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int pnp, qut;
+
+	for (pnp = 0; pnp <= 98; pnp++)
+	{
+		for (qut = pnp + 1; qut <= 99; qut++)
+		{
+			putchar((pnp / 10) + '0');
+			putchar((pnp % 10) + '0');
+			putchar((' ');
+			putchar((qut / 10) + '0');
+			putchar((qut % 10) + '0');
+			if (pnp == 98 && qut == 99)
+			continue;
+			putchar(',');
+			putchar(' ');
+			}
+			}
+			putchar('\n');
+			return (0);
+}
