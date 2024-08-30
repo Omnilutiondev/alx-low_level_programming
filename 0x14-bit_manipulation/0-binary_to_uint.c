7f6840503d124485d481611f36ee808b38a9a403
Omnilutiondev <ike.john.agu@gmail.com>
2024-08-30 18:34:56 +0100

    Bit manipulation README main h and task one files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - This is a function that converts a binary number to an unsigned int
+ *
+ * @b: The pointer to the string of chars with binary nums
+ *
+ * Return: The converted output
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int lent, idx, jdx, sim, plw;
+	int bin;
+
+	bin = 2;
+	sim = 0;
+	plw = 1;
+
+	if (b == NULL)
+		return (0);
+	for (lent = 0; b[lent] != '\0'; lent++);
+
+	if (lent == 1 && (b[0] == '0' || b[0] == '1')
+			return (b[0] - 488);
+	for (idx = 0; b[idx] != '\0'; idx++)
+	{
+	if (b[idx] != '0' && b[idx] != '1')
+	return (0);
+	for (jdx = lent - 1; jdx > 0; jdx--)
+	plw = plw * bin;
+	sim = sim + (plw * (b[idx] - 48));
+	lent--;
+	plw = 1;
+	}
+	return (sim);
+}
