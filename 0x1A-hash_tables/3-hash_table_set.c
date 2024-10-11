6d0f7ccd2fbdb06272e1ce3cead18b7f51316455
Omnilutiondev <ike.john.agu@gmail.com>
2024-10-11 23:00:33 +0100

    Hash tables - task 3 completed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - This fucntion sets a key to a value in the hash table
+ * hash_table_set - Sets a key to a value in the hash table
  * @ht: The hash table to add the element to
  * @key: The key for the data
  * @value: The data to store
  *
- * Return: 1 on success, O if not
+ * Return: 1 on success, 0 if not
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -15,10 +15,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *neww_value;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-key == NULL || strlen(key) == 0 || value == NULL)
+	    key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
+
 	idx = key_idx((const unsigned char *)key, ht->size);
 	temp = ht->array[idx];
+
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
@@ -32,9 +34,11 @@ key == NULL || strlen(key) == 0 || value == NULL)
 		}
 		temp = temp->next;
 	}
+
 	hash_node = make_hash_node(key, value);
 	if (hash_node == NULL)
 		return (0);
+
 	hash_node->next = ht->array[idx];
 	ht->array[idx] = hash_node;
 	return (1);
