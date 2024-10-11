942020682be0369ed611e026292f4f073ec3fb52
Omnilutiondev <ike.john.agu@gmail.com>
2024-10-11 23:38:09 +0100

    Hash tables - task 4 completed

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,14 +9,14 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx;
+	unsigned long int index;
 	hash_node_t *temp;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 	    key == NULL || strlen(key) == 0)
 		return (NULL);
-	idx = key_idx((const unsigned char *)key, ht->size);
-	temp = ht->array[idx];
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
