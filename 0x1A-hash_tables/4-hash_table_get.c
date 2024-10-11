e053eb3d21a33b0bb70c251afaae934cc488ca0c
Omnilutiondev <ike.john.agu@gmail.com>
2024-10-11 23:35:55 +0100

    Hash tables - task 4 completed

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - This function retrieves a value associated with a key
+ * @ht: The table to retrieve the value from
+ * @key: The key to find the value
+ *
+ * Return: The value associated with the key, or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *temp;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+	idx = key_idx((const unsigned char *)key, ht->size);
+	temp = ht->array[idx];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
