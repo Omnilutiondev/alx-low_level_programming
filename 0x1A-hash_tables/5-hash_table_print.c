a3a4b3de588ab55db3d94b96bd5263d5bbf1fca6
Omnilutiondev <ike.john.agu@gmail.com>
2024-10-14 12:28:22 +0100

    Hash Tables - Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - This function prints a hash table
+ * @ht: The hash table to be printed
+ *
+ * Return: Void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *temp;
+	char flaag = 0; /* 0 while there is no data to print */
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		temp = ht->array[idx];
+		while (temp != NULL)
+		{
+			if (flaag == 1)
+				printf(",");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flaag = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
