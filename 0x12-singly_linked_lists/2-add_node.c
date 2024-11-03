6b9a700efbfa55b467a95bf4d81d103f85c4f827
omnilutiondev <ike.john.agu@gmail.com>
2024-11-03 06:06:51 +0100

    fixed task 3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - This function adds a node to the head of the list
+ * @head: this is the pointer to the head node
+ * @str: the string field of the node
+ *
+ * Return: the size of the list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_ins = malloc(sizeof(list_t));
+
+	if (!head || !new_ins)
+		return (NULL);
+	if (str)
+	{
+		new_ins->str = strdup(str);
+		if (!new_ins->str)
+		{
+			free(new_ins);
+			return (NULL);
+		}
+		new_ins->len = _strlen(new_ins->str);
+	}
+
+	new_ins->next = *head;
+	*head = new_ins;
+	return (new_ins);
+}
