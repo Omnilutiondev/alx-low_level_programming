6772da4e10cc7c656c4af94792ae084099921862
omnilutiondev <ike.john.agu@gmail.com>
2024-11-03 07:01:23 +0100

    fixed task 4

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node_end - This function adds a node to the end of the list
+ * @head: The pointer for the node
+ * @str: The string to be added to the node
+ *
+ * Return: The size of the list or NULL if failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_end = malloc(sizeof(list_t));
+	list_t *end = *head;
+
+	if (!head || !new_end)
+		return (NULL);
+	if (str)
+	{
+		new_end->str = strdup(str);
+		if (!new_end->str)
+		{
+			free(new_end);
+			return (NULL);
+		}
+		new_end->len = strlen(new_node->str);
+	}
+	if (end)
+	{
+		while (end->next)
+			end = end->next;
+		end->next = new_end;
+	}
+	else
+		*head = new_end;
+	return (new_end);
+}
