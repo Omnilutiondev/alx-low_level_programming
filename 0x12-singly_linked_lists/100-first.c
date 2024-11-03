bc59e556a0e57705a3f40ed5f7b31a99f6944c81
omnilutiondev <ike.john.agu@gmail.com>
2024-11-03 08:44:47 +0100

    advanced task 1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
-void _constructor(void) __attribute__ ((constructor));
+void _newliner(void) __attribute__ ((newliner));
 
 /**
  * _constructor - This function executes the main()
  *
  * Return: Always void
  */
-void _constructor(void)
+void _newliner(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 		"I bore my house upon my back!\n");
