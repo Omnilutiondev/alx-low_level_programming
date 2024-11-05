06025ad2a26ed11768137c234f36500d1825768c
omnilutiondev <ike.john.agu@gmail.com>
2024-11-05 05:42:16 +0100

    Doubly linked lists - Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (!head || !new_node)
-		return(new_node ? free(new_node), NULL : NULL);
+		return (new_node ? free(new_node), NULL : NULL);
 
 	new_node->n = n;
 	new_node->prev = NULL;
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - This function adds a node at the end
+ * @head: The address of the head node
+ * @n: the integer
+ *
+ * Return: The address of the new node or NULL
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+
