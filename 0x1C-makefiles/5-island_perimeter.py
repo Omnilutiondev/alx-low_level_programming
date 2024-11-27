6f3383b9db1473e30a8f0466335bfc5dd3725547
omnilutiondev <ike.john.agu@gmail.com>
2024-11-27 20:49:21 +0100

    0x1C-Makefiles - Task 6 Completed

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,18 +3,18 @@
 
 def island_perimeter(grid):
     """return the perimeter in the grid for the island in question"""
-    heightt = len(grid)
-    breath = len(grid[0])
-    sizes = 0
+    height = len(grid)
+    width = len(grid[0])
+    size = 0
     edges = 0
 
-    for idx in range(heightt):
-        for jdx in range(breath):
+    for idx in range(height):
+        for jdx in range(width):
             if grid[idx][jdx] == 1:
-                sizes += 1
+                size += 1
                 if (jdx > 0 and grid[idx][jdx - 1] == 1):
                     edges += 1
                 if (idx > 0 and grid[idx - 1][jdx] == 1):
                     edges += 1
-    return ((sizes * 4) - (edges * 2))
+    return ((size * 4) - (edges * 2))
 
